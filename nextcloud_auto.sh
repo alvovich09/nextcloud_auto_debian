ac6424809f84ea283de39eae57b547ce52142ea7
alvovich09 <61094325+alvovich09@users.noreply.github.com>
2024-05-20 15:04:04 +0300

    add nginx and fix early bug

--- nextcloud_auto.sh
+++ nextcloud_auto.sh
@@ -16,28 +16,48 @@ deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib no
 deb http://deb.debian.org/debian bullseye-updates main contrib non-free
 deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free" >> ./sources.list
 
-#Устанавливаем бд mariadb
+#Устанавливаем БД mariadb
 sudo apt install mariadb-server -y
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
 
-#Сбор данных для создания БД
-echo -n "Введите пароль для администратора базы данных: "
-read password_base
-echo "password_base = $password_base" >> /home/debian/info.txt
+#Настраиваем и устанавливаем БД
 
 echo -n "Введите имя базы данных для создания: "
 read name_base
 echo "name_base = $name_base" >> /home/debian/info.txt
+mysql -u root -e "CREATE DATABASE $name_base DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
+echo "БД создана!"
 
 echo -n "Введите имя пользователя  для базы данных: "
-read name_user_base
+read  name_user_base
+echo -n "Введите пароль для пользователя созданного выше. Пароль будет скрыт: "
+read -s password_user_base
 echo "name_user_base = $name_user_base" >> /home/debian/info.txt
-
-echo -n "Введите пароль для пользователя созданного выше: "
-read password_user_base
 echo "password_user_base = $password_user_base" >> /home/debian/info.txt
+mysql -u root -e "CREATE USER ${name_user_base}@localhost IDENTIFIED BY '$password_user_base';"
+echo "Пользователь создан!"
 
-#Создание базы для Nextcloud
-sudo mysql -u root -p $password_base -e "CREATE DATABASE $name_base DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
-sudo mysql -u root -p $password_base -e "GRANT ALL PRIVILEGES ON $name_base.* TO $name_user_base@localhost IDENTIFIED BY '$password_user_base';"
+echo "Раздача всех прав пользователю на созданную базу данных"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $name_base.* TO $name_user_base@localhost IDENTIFIED BY '$password_user_base';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+echo "БД готова для продолжения установки"
+
+#Настраиваем репозиторий для установки. На момент создания скрипта, актуальный - 8.3
+
+#Устанавливаем пакеты для использования репозиториев по https
+sudo apt install -y lsb-release ca-certificates apt-transport-https software-properties-common gnupg2
+#Добавляем репозиторий
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/sury-php.list
+#Для нового репозитория загружаем специальный ключ безопасности
+wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add -
+#Обновляем список пакетов
+sudo apt update
+#Устанавливаем php
+sudo -y install php8.3
+#Устанавливаем дополнительные модули для взаимодействия PHP-Nextcloud
+sudo apt install libapache2-mod-php php-gd php-mysql php-curl php-mbstring php-intl php-gmp php-bcmath php-xml php-imagick php-zip
+
+#Установка и настройка nginx
+
+sudo apt install nginx -y
